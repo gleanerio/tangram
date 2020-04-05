89d191af36d30589eebb50d1ebaa6290943381b9
datadavev <datadavev@users.noreply.github.com>
2020-04-05 11:50:21 -0400

    Simplified and removed spurious code

--- tangram_web.py
+++ tangram_web.py
@@ -1,18 +1,13 @@
 import os
-from flask import Flask, Response, request, redirect
+import flask #from flask import Flask, Response, request, redirect
 from flask.views import MethodView
 from flask_cors import CORS
-from pyshacl import validate
 import rdflib
-import requests
-from bs4 import BeautifulSoup
-import urllib
-import urllib.request
-import logging
+from pyshacl import validate
 
 from flasgger import Swagger
 
-app = Flask(__name__,
+app = flask.Flask(__name__,
             static_url_path='',
             static_folder='web/static',
             template_folder='web/templates')
@@ -26,8 +21,8 @@ swagger_template = {
         "description":"Service for evaluating schema.org content against ESIP Science on schema.org guidelines.",
         "version":"0.2.0",
     },
+    "basePath":"/dev",
     "schemes": [
-        "http",
         "https"
     ]
 }
@@ -45,51 +40,8 @@ OUTPUT_FORMATS = {
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Add in a function that reads the local shape and returns human or machine response
-# It will be method GET with ?url=URL&format=[human,machine]&shape=[required,recommended]
-# /rescheck
-@app.route('/ucheck', methods=['GET'])
-def netcheck():
-    if request.method == 'GET':
-        u = request.args.get('url')  # if key doesn't exist, returns None
-        f = request.args.get('format')  # if key doesn't exist, returns None
-        s = request.args.get('shape')  # if key doesn't exist, returns None
-
-        # read in shape graph
-        if s == 'recommended':
-            sg = open("./shapes/googleRecommended.ttl", 'r')
-        else:
-            sg = open("./shapes/googleRequired.ttl", 'r')
-
-        s = rdflib.Graph()
-        sr = s.parse(sg, format="ttl")
-
-        html = urllib.request.urlopen(u).read()
-        d = rdflib.Graph()
-
-        if ".jsonld" in u:
-            with urllib.request.urlopen(u) as response:
-                jld = response.read()
-            dr = d.parse(data=jld, format="json-ld")
-
-        else:
-            soup = BeautifulSoup(html, "html.parser")
-            p = soup.find('script', {'type': 'application/ld+json'})
-            dr = d.parse(data=p.contents[0], format="json-ld")
-
-        # call pySHACL
-        conforms, v_graph, v_text = validate(dr, shacl_graph=sr,
-                                             data_graph_format="json-ld",
-                                             shacl_graph_format="ttl",
-                                             inference='none', debug=False,
-                                             serialize_report_graph=False)
-
-        # I default to robot, but then never both to test that  :(
-        if f == 'human':
-            return '{} {}'.format(conforms, v_text)
-        else:
-            return v_graph.serialize(format="nt")
-
+def response422(msg):
+    return flask.Response(status=422, response=msg)
 
 class VerifyView(MethodView):
 
@@ -109,11 +61,11 @@ class VerifyView(MethodView):
                                              inference=inference, debug=False,
                                              serialize_report_graph=False)
         if out_format == 'human':
-            return Response(v_text, mimetype="text/plain")
+            return flask.Response(v_text, mimetype="text/plain")
         else:
             skolemized = v_graph.skolemize(authority="http://ld.geoschemas.org", basepath="/")
             skolemized.namespace_manager = v_graph.namespace_manager
-            return Response(skolemized.serialize(format=out_format), mimetype=OUTPUT_FORMATS[out_format])
+            return flask.Response(skolemized.serialize(format=out_format), mimetype=OUTPUT_FORMATS[out_format])
 
 
     def get(self):
@@ -125,17 +77,17 @@ class VerifyView(MethodView):
         json-ld and the SHACL graph in turtle.
         ---
         parameters:
-          - name: "datagraph"
+          - name: "dg"
             in: "query"
             required: true
             description: "URL for data graph source"
             default: "https://raw.githubusercontent.com/datadavev/science-on-schema.org/2020-SOSOV/validation/testingDataGraphs/ds_badns1.json"
-          - name: "shapegraph"
+          - name: "sg"
             in: "query"
             required: true
             description: "URL for SHACL graph source"
             default: "https://raw.githubusercontent.com/datadavev/science-on-schema.org/2020-SOSOV/validation/shapes/so_namespace.ttl"
-          - name: format
+          - name: fmt
             in: "query"
             description: Format of response
             type: "string"
@@ -154,23 +106,23 @@ class VerifyView(MethodView):
         '''
         data_graph = None
         shacl_graph = None
-        out_format = request.args.get('format', 'human')
+        out_format = flask.request.args.get('fmt', 'human')
         if out_format not in OUTPUT_FORMATS.keys():
-            return Response(status=422, response=f"Unrecognized format requested: {out_format}")
+            return response422(f"Unrecognized format requested: {out_format}")
         try:
             data_graph = rdflib.ConjunctiveGraph()
-            data_graph.parse(request.args['datagraph'], format="json-ld")
+            data_graph.parse(flask.request.args['dg'], format="json-ld")
         except KeyError as e:
-            return Response(status=422, response="Data graph URL is required.")
+            return response422("Data graph URL is required.")
         except Exception as e:
-            return Response(status=422, response=str(e))
+            return response422(str(e))
         try:
             shacl_graph = rdflib.ConjunctiveGraph()
-            shacl_graph.parse(request.args['shapegraph'], format="turtle")
+            shacl_graph.parse(flask.request.args['sg'], format="turtle")
         except KeyError as e:
-            return Response(status=422, response="SHACL shape graph URL is required.")
+            return response422("SHACL shape graph URL is required.")
         except Exception as e:
-            return Response(status=422, response=str(e))
+            return response422(str(e))
         return self.doVerification(data_graph, shacl_graph, out_format)
 
 
@@ -190,17 +142,17 @@ class VerifyView(MethodView):
           - application/ld+json
         parameters:
           - in: formData
-            name: datagraph
+            name: dg
             description: Data graph file
             type: file
             required: true
           - in: formData
-            name: shapegraph
+            name: sg
             description: SHACL shape graph file
             type: file
             required: true
           - in: formData
-            name: format
+            name: fmt
             description: Format of response
             type: "string"
             enum: ['human', 'json-ld', 'turtle', 'nt', 'n3']
@@ -214,24 +166,24 @@ class VerifyView(MethodView):
         '''
         data_graph = None
         shacl_graph = None
-        out_format = request.form.get('format', 'human')
+        out_format = flask.request.form.get('fmt', 'human')
         app.logger.debug("out_format = %s", out_format)
         if out_format not in OUTPUT_FORMATS.keys():
-            return Response(status=422, response=f"Unrecognized format requested: {out_format}")
+            return response422(f"Unrecognized format requested: {out_format}")
         try:
             data_graph = rdflib.ConjunctiveGraph()
-            data_graph.parse(request.files['datagraph'], format="json-ld")
+            data_graph.parse(flask.request.files['dg'], format="json-ld")
         except KeyError as e:
-            return Response(status=422, response="Data graph file is required.")
+            return response422("Data graph file is required.")
         except Exception as e:
-            return Response(status=422, response=str(e))
+            return response422(str(e))
         try:
             shacl_graph = rdflib.ConjunctiveGraph()
-            shacl_graph.parse(request.files['shapegraph'], format="turtle")
+            shacl_graph.parse(flask.request.files['sg'], format="turtle")
         except KeyError as e:
-            return Response(status=422, response="SHACL shape graph file is required.")
+            return response422("SHACL shape graph file is required.")
         except Exception as e:
-            return Response(status=422, response=str(e))
+            return response422(str(e))
         return self.doVerification(data_graph, shacl_graph, out_format)
 
 
@@ -240,7 +192,7 @@ app.add_url_rule('/verify', view_func=VerifyView.as_view('verify'))
 
 @app.route("/")
 def index():
-    return redirect("/apidocs")
+    return flask.redirect("apidocs")
 
 
 if __name__ == "__main__":
