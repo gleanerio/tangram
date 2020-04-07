7d2c071cfb817f9008f2f9f209156180e2432251
datadavev <datadavev@users.noreply.github.com>
2020-04-07 09:24:02 -0400

    closes #2, closes #3

--- src/tangram_web.py
+++ src/tangram_web.py
@@ -5,50 +5,52 @@ import flask
 from flask.views import MethodView
 from flask_cors import CORS
 import rdflib
-from pyshacl import validate
+import sosov.verify
 from flasgger import Swagger
 from extruct.jsonld import JsonLdExtractor
 
 IS_LAMBDA = False
 s3_client = None
 
-if 'SERVERTYPE' in os.environ and os.environ['SERVERTYPE'] == 'AWS Lambda':
+if "SERVERTYPE" in os.environ and os.environ["SERVERTYPE"] == "AWS Lambda":
     import boto3
+
     IS_LAMBDA = True
     s3_client = boto3.client("s3")
 
-app = flask.Flask(__name__,
-            static_url_path='',
-            static_folder='web/static',
-            template_folder='web/templates')
-app.config['SWAGGER'] = {
-    'title':'Tangram',
+app = flask.Flask(
+    __name__,
+    static_url_path="",
+    static_folder="web/static",
+    template_folder="web/templates",
+)
+
+app.config["SWAGGER"] = {
+    "title": "Tangram",
 }
+
 swagger_template = {
     "info": {
         "title": "Tangram SHACL Verification",
-        "description":"Service for evaluating schema.org content against ESIP Science on schema.org guidelines.",
-        "version":"0.2.0",
+        "description": "Service for evaluating schema.org content against ESIP Science on schema.org guidelines.",
+        "version": "0.2.0",
     },
-    "basePath":os.environ.get('lambda_base_path', ''),
-    "schemes": [
-        "https","http"
-    ]
+    "basePath": os.environ.get("lambda_base_path", ""),
+    "schemes": ["https", "http"],
 }
-
 
 swagger = Swagger(app, template=swagger_template)
 
 OUTPUT_FORMATS = {
-    'human': 'text/plain',
-    'json-ld':'application/ld+json',
-    'turtle':'text/turtle',
-    'nt':'text/plain',
-    'n3':'text/n3',
+    "human": "text/plain",
+    "json-ld": "application/ld+json",
+    "turtle": "text/turtle",
+    "nt": "text/plain",
+    "n3": "text/n3",
 }
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
 
 def getSchemaOrgTurtlePath():
@@ -75,32 +77,33 @@ def getJsonLdFromHTML(html_text):
 
 
 class VerifyView(MethodView):
-
-    def doVerification(self, data_graph, shacl_graph, out_format, needs_schemaorg=False):
-        # call pySHACL
+    def doVerification(
+        self, data_graph, shacl_graph, out_format, needs_schemaorg=False
+    ):
         ont_graph = None
-        inference = "none"
-        if needs_schemaorg:
-            # TODO: handle this
-            # Load schema.org graph
-            # set inference to 'rdfs'
-            pass
-        conforms, v_graph, v_text = validate(data_graph, shacl_graph=shacl_graph,
-                                             data_graph_format="json-ld",
-                                             shacl_graph_format="ttl",
-                                             ont_graph=ont_graph,
-                                             inference=inference, debug=False,
-                                             serialize_report_graph=False)
-        if out_format == 'human':
+        advanced = False
+        meta_shacl = False
+        conforms, v_graph, v_text = sosov.verify.validateSHACL(
+            data_graph,
+            shacl_graph=shacl_graph,
+            ont_graph=ont_graph,
+            meta_shacl=meta_shacl,
+            advanced=advanced,
+        )
+        if out_format == "human":
             return flask.Response(v_text, mimetype="text/plain")
         else:
-            skolemized = v_graph.skolemize(authority="http://ld.geoschemas.org", basepath="/")
+            skolemized = v_graph.skolemize(
+                authority="http://ld.geoschemas.org", basepath="/"
+            )
             skolemized.namespace_manager = v_graph.namespace_manager
-            return flask.Response(skolemized.serialize(format=out_format), mimetype=OUTPUT_FORMATS[out_format])
-
+            return flask.Response(
+                skolemized.serialize(format=out_format),
+                mimetype=OUTPUT_FORMATS[out_format],
+            )
 
     def get(self):
-        '''
+        """
         Perform SHACL validation on provided data and SHACL sources.
 
         The locations of the data graph and shacl shape graph are provided as URLs, and
@@ -141,43 +144,46 @@ class VerifyView(MethodView):
             description: Missing or invalid input data
           200:
             description: Result of SHACL evaluation
-        '''
+        """
         app.logger.debug("GET on verify")
         data_graph = None
         shacl_graph = None
-        out_format = flask.request.args.get('fmt', 'human')
+        out_format = flask.request.args.get("fmt", "human")
         app.logger.debug("out_format = %s", out_format)
         if out_format not in OUTPUT_FORMATS.keys():
             return response422(f"Unrecognized format requested: {out_format}")
         try:
             data_graph = rdflib.ConjunctiveGraph()
-            data_format = flask.request.args['df']
-            data_url = flask.request.args['dg']
-            if data_format == 'html':
+            data_format = flask.request.args["df"]
+            data_url = flask.request.args["dg"]
+            if data_format == "html":
                 response = requests.get(data_url)
                 if response.status_code != 200:
-                    raise ValueError(f"Bad response ({response.status_code}) from dg = {data_url}")
+                    raise ValueError(
+                        f"Bad response ({response.status_code}) from dg = {data_url}"
+                    )
                 json_content = getJsonLdFromHTML(response.text)
                 for json_data in json_content:
-                    data_graph.parse(data=json.dumps(json_data), format="json-ld", publicID=data_url)
+                    data_graph.parse(
+                        data=json.dumps(json_data), format="json-ld", publicID=data_url
+                    )
             else:
-                data_graph.parse(flask.request.args['dg'], format="json-ld")
+                data_graph.parse(flask.request.args["dg"], format="json-ld")
         except KeyError as e:
             return response422("Data graph URL is required.")
         except Exception as e:
             return response422(str(e))
         try:
             shacl_graph = rdflib.ConjunctiveGraph()
-            shacl_graph.parse(flask.request.args['sg'], format="turtle")
+            shacl_graph.parse(flask.request.args["sg"], format="turtle")
         except KeyError as e:
             return response422("SHACL shape graph URL is required.")
         except Exception as e:
             return response422(str(e))
         return self.doVerification(data_graph, shacl_graph, out_format)
 
-
     def post(self):
-        '''
+        """
         Perform SHACL validation on provided data and SHACL shape graphs.
 
         The data graph and SHACL shape graph are provided as file uploads.
@@ -213,24 +219,24 @@ class VerifyView(MethodView):
           200:
             description: Result of SHACL evaluation
 
-        '''
+        """
         app.logger.debug("POST on verify")
         data_graph = None
         shacl_graph = None
-        out_format = flask.request.form.get('fmt', 'human')
+        out_format = flask.request.form.get("fmt", "human")
         app.logger.debug("out_format = %s", out_format)
         if out_format not in OUTPUT_FORMATS.keys():
             return response422(f"Unrecognized format requested: {out_format}")
         try:
             data_graph = rdflib.ConjunctiveGraph()
-            data_graph.parse(flask.request.files['dg'], format="json-ld")
+            data_graph.parse(flask.request.files["dg"], format="json-ld")
         except KeyError as e:
             return response422("Data graph file is required.")
         except Exception as e:
             return response422(str(e))
         try:
             shacl_graph = rdflib.ConjunctiveGraph()
-            shacl_graph.parse(flask.request.files['sg'], format="turtle")
+            shacl_graph.parse(flask.request.files["sg"], format="turtle")
         except KeyError as e:
             return response422("SHACL shape graph file is required.")
         except Exception as e:
@@ -238,12 +244,12 @@ class VerifyView(MethodView):
         return self.doVerification(data_graph, shacl_graph, out_format)
 
 
-app.add_url_rule('/verify', view_func=VerifyView.as_view('verify'))
+app.add_url_rule("/verify", view_func=VerifyView.as_view("verify"))
 
 
 @app.route("/extract")
 def getHtmlFromUrl():
-    '''
+    """
     Return json-ld extracted from a URL.
 
     Attempts to extract json-ld from the HTML at the provided URL. The response is an array of
@@ -262,18 +268,21 @@ def getHtmlFromUrl():
         description: "Something went wrong retrieving content from the URL"
       200:
         description: "Array of retrieved json-ld content"
-    '''
-    url = flask.request.args['url']
+    """
+    url = flask.request.args["url"]
     app.logger.debug("requested url = %s", url)
     response = requests.get(url)
     if response.status_code == 200:
-        return flask.Response(response=json.dumps(getJsonLdFromHTML(response.text)), mimetype=OUTPUT_FORMATS['json-ld'])
+        return flask.Response(
+            response=json.dumps(getJsonLdFromHTML(response.text)),
+            mimetype=OUTPUT_FORMATS["json-ld"],
+        )
     return response422(f"Bad response ({response.status_code}) from url = {url}")
 
 
 @app.route("/so")
 def getSchemaOrg():
-    '''
+    """
     Return the cached copy of schema.org RDF in Turtle format.
 
     A copy of the schema.org RDF with the namespace expressed in "https://schema.org/" is cached for
@@ -284,9 +293,12 @@ def getSchemaOrg():
     responses:
       200:
         description: The schema.org RDF in Turtle
-    '''
+    """
     so_turtle = getSchemaOrgTurtlePath()
-    return flask.Response(open(so_turtle,"rb").read(), mimetype=OUTPUT_FORMATS["turtle"])
+    return flask.Response(
+        open(so_turtle, "rb").read(), mimetype=OUTPUT_FORMATS["turtle"]
+    )
+
 
 @app.route("/")
 def index():
@@ -294,4 +306,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
